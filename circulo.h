c35773eaecad8493af735020e83a1d6d2593940c
AnahiTupiza <dtupizat@est.ups.edu.ec>
2022-07-18 10:18:46 -0500

    Agregar circunferencia

--- circulo.h
+++ circulo.h
@@ -2,12 +2,38 @@
 #define CIRCULO_H
 
 #include <QPoint>
+#include <QColor>
+#include <QPainter>
+#include <QColorDialog>
+
+//#include<QGraphicsScene>
+
 
 class Circulo : public QPoint
 {
+    Q_OBJECT
+
 public:
     Circulo();
     Circulo(int xpos, int ypos);
+
+    Circulo (QWidget *parent =nullptr);
+    ~Circulo ();
+
+
+
+private:
+    void arriba ();
+    void abajo ();
+    void izquierda ();
+    void derecha ();
+
+    QPoint mInicial;
+    QPoint mFinal;
+    QPoint mCentro;
+    int mAncho;
+    QColor mColor;
+
 };
 
 #endif // CIRCULO_H
